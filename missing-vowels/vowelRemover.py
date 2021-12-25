aa8b19a62ae4c990e4b7e0f69777f99cf2fa3382
Meep3278 <30444515+Meep3278@users.noreply.github.com>
2021-12-25 03:24:23 -0800

    Update vowelRemover.py

--- missing-vowels/vowelRemover.py
+++ missing-vowels/vowelRemover.py
@@ -1,18 +1,20 @@
-#include <stdio.h>
 import random
-import sys
 
-# input = sys.argv[1]
+ignoreChars = ".,'aeiou "
+
 file = open('input.txt', 'r')
 lines = file.readlines()
-for input in lines:
-    output = ""
-    for ch in input:
-        if(ch=='.' or ch==',' or ch=='\'' or ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I' or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u' or ch==' '):
+file.close()
+output = ""
+for line in lines:
+    for ch in line.lower().strip('\n'):
+        if ch in ignoreChars:
             continue
-        add_ch = ch.capitalize()
+        add_ch = ch.upper()
         rand_num = random.randint(0, 4)
         if rand_num == 0:
             add_ch += " "
         output += add_ch
-    print(output)
+    output += '\n'
+
+print(output, end = '')
